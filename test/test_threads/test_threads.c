df6d2e0ef724718eae2690170bbee2226689deb7
Henry Cope <henrydaviscope@gmail.com>
2023-09-25 16:13:14 -0600

    added test that loop runs

--- test/test_threads/test_threads.c
+++ test/test_threads/test_threads.c
@@ -27,9 +27,22 @@ void test_lock(void)
 
     TEST_ASSERT_EQUAL(1,result);
     TEST_ASSERT_EQUAL(0, counter);
+}
+void test_loop_runs(void)
+{
 
+    // Take semaphore for 500 Msec, try to take during lock and after given
 
+    struct k_sem sem;
+    int counter = 0;
+	struct k_timer timer;
+	k_timer_init(&timer, NULL, NULL);
+    k_sem_init(&sem, 1, 1);
 
+    int result = do_loop(&timer, &sem, &counter, "main", K_MSEC(100));
+
+    TEST_ASSERT_EQUAL(0,result);
+    TEST_ASSERT_EQUAL(1, counter);
 }
 
 
@@ -37,5 +50,6 @@ int main (void)
 {
     UNITY_BEGIN();
     RUN_TEST(test_lock);
+    RUN_TEST(test_loop_runs);
     return UNITY_END();
 }
